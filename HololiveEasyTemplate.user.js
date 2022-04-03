cadc45a39373e8571717950c2df24cf9a76a199a
Johnson Chen <cyberiumdev@gmail.com>
2022-04-03 22:05:42 +1000

    Add auto-collation of template images

--- HololiveEasyTemplate.user.js
+++ HololiveEasyTemplate.user.js
@@ -6,53 +6,53 @@
 // @author       TellowKrinkle
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @grant        none
+// @grant        GM_xmlhttpRequest
+// @connect      githubusercontent.com
+
 // ==/UserScript==
 if (window.top !== window.self) {
-	window.addEventListener('load', () => {
-		let templates;
-		const opacity = 1;
-		const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
-		const canvas = camera.querySelector("mona-lisa-canvas");
-		const container = canvas.shadowRoot.querySelector('.container');
-		function addImage(src, posX, posY) {
-			let img = document.createElement("img");
-			img.onload = () => {
-				const width = img.width;
-				const height = img.height;
-				const canvas = document.createElement("canvas");
-				canvas.width = width * 3;
-				canvas.height = height * 3;
-				canvas.style = `position: absolute; left: ${posX}px; top: ${posY}px; image-rendering: pixelated; width: ${width}px; height: ${height}px;`;
-				const ctx = canvas.getContext("2d");
-				ctx.globalAlpha = opacity;
-				for (let y = 0; y < height; y++) {
-					for (let x = 0; x < width; x++) {
-						ctx.drawImage(img, x, y, 1, 1, x * 3 + 1, y * 3 + 1, 1, 1);
-					}
-				}
-				container.appendChild(canvas);
-			};
-			img.src = src;
-		}
-		function setTemplate(document) {
-			templates = document
-		}
-		function httpGetAsync(theUrl, callback)
-			{
-				var xmlHttp = new XMLHttpRequest();
-				xmlHttp.onreadystatechange = function() { 
-					if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-						callback(xmlHttp.responseText);
-				}
-				xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-				xmlHttp.send(null);
-			}
+    window.addEventListener('load', () => {
+        const opacity = 1;
+        const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
+        const canvas = camera.querySelector("mona-lisa-canvas");
+        const container = canvas.shadowRoot.querySelector('.container');
+        function addImage(src, posX, posY) {
+            let img = document.createElement("img");
+            img.onload = () => {
+                const width = img.width;
+                const height = img.height;
+                const canvas = document.createElement("canvas");
+                canvas.width = width * 3;
+                canvas.height = height * 3;
+                canvas.style = `position: absolute; left: ${posX}px; top: ${posY}px; image-rendering: pixelated; width: ${width}px; height: ${height}px;`;
+                const ctx = canvas.getContext("2d");
+                ctx.globalAlpha = opacity;
+                for (let y = 0; y < height; y++) {
+                    for (let x = 0; x < width; x++) {
+                        ctx.drawImage(img, x, y, 1, 1, x * 3 + 1, y * 3 + 1, 1, 1);
+                    }
+                }
+                container.appendChild(canvas);
+            };
+            img.src = src;
+        }
+        GM_xmlhttpRequest({
+            method: 'GET',
+            fetch: true,
+            responseType: 'document',
+            url: 'https://raw.githubusercontent.com/Styml/holotemplate/main/all-templates.csv',
+            onload: function (response) {
+                const responses = response.responseText.split('\n')
+                for (let i = 0; i < responses.length; i++) {
+                    let templateObj = {
+                        url: responses[i].split(',')[0],
+                        xcoord: responses[i].split(',')[1],
+                        ycoord: responses[i].split(',')[2]
+                    }
 
-		templates = httpGetAsync("https://raw.githubusercontent.com/DutchEllie/holotemplate/main/all-templates.csv", setTemplate)
-		console.log(templates)
-		/* parse CSV here */
-		
-	}, false);
+                    addImage(templateObj.url, templateObj.xcoord, templateObj.ycoord);
+                }
+            }
+        });
+    }, false);
 }
-
